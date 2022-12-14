dae32f97cbebf31b697152d9777c7949bc3df05d
Pradeep Poudel <prdppoudel123@icloud.com>
2022-12-14 21:56:08 +1100

    Initial Commit

---
+++ PeePee/PeePeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeePeeApp.swift
+//  PeePee
+//
+//  Created by Prdp Poudel on 14/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct PeePeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
